ae48d296e2d5c5efbb30e3bd7f2963f4b8a47105
acq17-ua <acq17@gcloud.ua.es>
2025-02-20 10:40:18 +0100

    main para testear tokenizador de 1 archivo

--- p1/proyecto/src/main.cpp
+++ p1/proyecto/src/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include "../include/tokenizador.h"
+#include <list>
+#include <fstream>
+#include <chrono>
 using namespace std;
 
-int main(int argc, char** argv) {
-
+int main(int argc, char** argv)
+{
 	Tokenizador t = Tokenizador();	
+
+	t.Tokenizar("input.txt");
+
+	list<string>::const_iterator i;
 
 
 	return 0;
