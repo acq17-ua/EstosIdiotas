fef45b325b109d120d321ad971380de0122848f6
acq17-ua <acq17@gcloud.ua.es>
2025-03-20 13:59:42 +0100

    setup

--- p2/proyecto/lib/indexadorHash.cpp
+++ p2/proyecto/lib/indexadorHash.cpp
@@ -1,1 +1,79 @@
 #include "indexadorHash.h"
+
+IndexadorHash::IndexadorHash(	const string& fichStopWords,   
+	const string& delimitadores,   
+	const bool& detectComp, 		 
+	const bool& minuscSinAcentos,  	
+	const string& dirIndice, 		
+	const int& tStemmer, 			
+	const bool& almPosTerm) {} 		
+
+IndexadorHash::IndexadorHash() {}	
+
+IndexadorHash::IndexadorHash(const string& directorioIndexacion) {}
+
+IndexadorHash::IndexadorHash(const IndexadorHash&) {}
+
+IndexadorHash::~IndexadorHash() {}
+
+IndexadorHash& IndexadorHash::operator= (const IndexadorHash&) {}
+
+bool IndexadorHash::Indexar(const string& ficheroDocumentos) {}
+
+bool IndexadorHash::IndexarDirectorio(const string& dirAIndexar) {}
+
+bool IndexadorHash::GuardarIndexacion() const {}
+
+bool IndexadorHash::RecuperarIndexacion (const string& directorioIndexacion) {}
+
+bool IndexadorHash::IndexarPregunta(const string& preg) {}
+
+bool IndexadorHash::DevuelvePregunta(string& preg) const {}
+
+bool IndexadorHash::DevuelvePregunta(const string& word, InformacionTerminoPregunta& inf) const {} 
+
+bool IndexadorHash::DevuelvePregunta(InformacionPregunta& inf) const {} 
+
+bool IndexadorHash::Devuelve(const string& word, InformacionTermino& inf) const {} 
+
+bool IndexadorHash::Devuelve(const string& word, const string& nomDoc, InfTermDoc& InfDoc) const {}  
+
+bool IndexadorHash::Existe(const string& word) const {}	 
+
+bool IndexadorHash::BorraDoc(const string& nomDoc) {} 
+
+void IndexadorHash::VaciarIndiceDocs() {} 
+
+void IndexadorHash::VaciarIndicePreg() {} 
+
+int IndexadorHash::NumPalIndexadas() const {} 
+
+string IndexadorHash::DevolverFichPalParada () const {} 
+
+void IndexadorHash::ListarPalParada() const {} 
+
+int IndexadorHash::NumPalParada() const {} 
+
+string IndexadorHash::DevolverDelimitadores () const {} 
+
+bool IndexadorHash::DevolverCasosEspeciales () const {}
+
+bool IndexadorHash::DevolverPasarAminuscSinAcentos () const {}
+
+bool IndexadorHash::DevolverAlmacenarPosTerm () const {}
+
+string IndexadorHash::DevolverDirIndice () const {} 
+
+int IndexadorHash::DevolverTipoStemming () const {} 
+
+void IndexadorHash::ListarInfColeccDocs() const {} 
+
+void IndexadorHash::ListarTerminos() const {} 
+
+bool IndexadorHash::ListarTerminos(const string& nomDoc) const {} 
+
+void IndexadorHash::ListarDocs() const {} 
+
+bool IndexadorHash::ListarDocs(const string& nomDoc) const {} 
+
+
--- p2/proyecto/lib/indexadorInformacion.cpp
+++ p2/proyecto/lib/indexadorInformacion.cpp
@@ -1,1 +1,31 @@
 #include "indexadorInformacion.h"
+
+InfTermDoc::InfTermDoc (const InfTermDoc& ) {}
+InfTermDoc::InfTermDoc () {}		
+InfTermDoc::~InfTermDoc () {}		
+InfTermDoc& InfTermDoc::operator= (const InfTermDoc& ) {}
+
+InformacionTermino::InformacionTermino(const InformacionTermino &) {}
+InformacionTermino::InformacionTermino() {}		
+InformacionTermino::~InformacionTermino() {}		
+InformacionTermino& InformacionTermino::operator=(const InformacionTermino &) {}
+
+InfDoc::InfDoc(const InfDoc &) {}
+InfDoc::InfDoc() {}	
+InfDoc::~InfDoc() {}
+InfDoc& InfDoc::operator=(const InfDoc &) {}
+
+InfColeccionDocs::InfColeccionDocs (const InfColeccionDocs &) {}
+InfColeccionDocs::InfColeccionDocs () {}
+InfColeccionDocs::~InfColeccionDocs () {}
+InfColeccionDocs& InfColeccionDocs::operator= (const InfColeccionDocs &) {}
+
+InformacionTerminoPregunta::InformacionTerminoPregunta (const InformacionTerminoPregunta &) {}
+InformacionTerminoPregunta::InformacionTerminoPregunta () {}
+InformacionTerminoPregunta::~InformacionTerminoPregunta () {}
+InformacionTerminoPregunta& InformacionTerminoPregunta::operator= (const InformacionTerminoPregunta &) {}
+
+InformacionPregunta::InformacionPregunta (const InformacionPregunta &) {}
+InformacionPregunta::InformacionPregunta () {}	
+InformacionPregunta::~InformacionPregunta () {}
+InformacionPregunta& InformacionPregunta::operator= (const InformacionPregunta &) {}
